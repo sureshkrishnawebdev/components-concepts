6228a99d799507cf50f68187054221369425701d
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-03-22 22:43:03 +0530

    step 8 - basic unit testing implemented - deep level testing pending

--- src/inputs/parent-input/parent-input.component.spec.ts
+++ src/inputs/parent-input/parent-input.component.spec.ts
@@ -1,14 +1,20 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { FaInputComponent } from '../fa-input/fa-input.component';
+import { InputRefDirective } from '../shared/directives/input-ref.directive';
 
 import { ParentInputComponent } from './parent-input.component';
 
-describe('ParentInputComponent', () => {
-  let component: ParentInputComponent;
-  let fixture: ComponentFixture<ParentInputComponent>;
+fdescribe('ParentInputComponent', () => {
+  let component: ParentInputComponent,
+   fixture: ComponentFixture<ParentInputComponent>,
+   el: DebugElement,
+   emailField: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ParentInputComponent ]
+      declarations: [ ParentInputComponent, FaInputComponent, InputRefDirective]
     })
     .compileComponents();
   });
@@ -16,10 +22,28 @@ describe('ParentInputComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ParentInputComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
+    emailField = el.query( By.css('#email-field') );
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create a font awesome email input', () => {
+    expect(emailField).toBeTruthy();
+  });
+
+  it('should create an icon for font awesome email input', () => {
+    let icon = emailField.query( By.css('i.icon.fa.fa-envelope') )
+    expect(icon).toBeTruthy();
+  });
+
+  it('should have projected the correct test input inisde the email field', () => {
+    let input = emailField.query( By.css('input.test-class') )
+    expect(input).toBeTruthy();
+  });
+
 });
+
